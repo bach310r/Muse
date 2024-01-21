f7520baa72a4d4ad9b044f0c7f1417e5c7e36595
Gunnar <gnnrbachelor@gmail.com>
2024-01-21 10:46:50 -0700

    Add route and template for main page

--- pdf_gen/settings.py
+++ pdf_gen/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user_log.apps.UserLogConfig',
+    'generator.apps.GeneratorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
